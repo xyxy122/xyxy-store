c78681ea574b9ab6e1731be6f6b78c4528303f36
Xyxy12 <165573445+xyxy122@users.noreply.github.com>
2025-07-12 15:09:09 +0700

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -34,16 +34,73 @@ function App() {
   const [promoId, setPromoId] = useState(null);
 
   const games = [
-    { id: 1, title: "Cyberpunk 2077", price: 10000, image: "https://cdn.cloudflare.steamstatic.com/steam/apps/1091500/header.jpg" },
-    { id: 2, title: "Red Dead Redemption 2", price: 10000, image: "https://cdn.cloudflare.steamstatic.com/steam/apps/1174180/header.jpg" },
-    { id: 3, title: "Elden Ring", price: 10000, image: "https://cdn.cloudflare.steamstatic.com/steam/apps/1245620/header.jpg" },
-    { id: 4, title: "Assassins Creed IV", price: 10000, image: "https://wallpapercave.com/wp/wp3076360.jpg" },
-    { id: 5, title: "Batman Arkham Origins", price: 10000, image: "https://www.nintendo.com/eu/media/images/10_share_images/games_15/wiiu_14/SI_WiiU_BatmanArkhamOrigins_image1600w.jpg" },
-    { id: 6, title: "Persona 5 : Royale", price: 10000, image: "https://www.gamereactor.eu/media/33/persona5royal_3113393b.jpg" },
-    { id: 7, title: "Balatro", price: 10000, image: "https://static0.gamerantimages.com/wordpress/wp-content/uploads/2024/08/balatro-1.jpg" },
-    { id: 8, title: "Tekken 7", price: 10000, image: "https://media.graphassets.com/jHYvjBVjT3OnOuPQrTfs" },
-    { id: 9, title: "Tekken 8", price: 10000, image: "https://akinseagleseye.com/wp-content/uploads/2024/02/tekken-8-artwork.jpg" },
-    { id: 10, title: "Among Us", price: 10000, image: "https://static1.srcdn.com/wordpress/wp-content/uploads/2020/11/Among-Us-217-million.jpg" },
+    {
+      id: 1,
+      title: "Cyberpunk 2077",
+      price: 10000,
+      image:
+        "https://cdn.cloudflare.steamstatic.com/steam/apps/1091500/header.jpg",
+    },
+    {
+      id: 2,
+      title: "Red Dead Redemption 2",
+      price: 10000,
+      image:
+        "https://cdn.cloudflare.steamstatic.com/steam/apps/1174180/header.jpg",
+    },
+    {
+      id: 3,
+      title: "Elden Ring",
+      price: 10000,
+      image:
+        "https://cdn.cloudflare.steamstatic.com/steam/apps/1245620/header.jpg",
+    },
+    {
+      id: 4,
+      title: "Assassins Creed IV",
+      price: 10000,
+      image: "https://wallpapercave.com/wp/wp3076360.jpg",
+    },
+    {
+      id: 5,
+      title: "Batman Arkham Origins",
+      price: 10000,
+      image:
+        "https://www.nintendo.com/eu/media/images/10_share_images/games_15/wiiu_14/SI_WiiU_BatmanArkhamOrigins_image1600w.jpg",
+    },
+    {
+      id: 6,
+      title: "Persona 5 : Royale",
+      price: 10000,
+      image: "https://www.gamereactor.eu/media/33/persona5royal_3113393b.jpg",
+    },
+    {
+      id: 7,
+      title: "Balatro",
+      price: 10000,
+      image:
+        "https://static0.gamerantimages.com/wordpress/wp-content/uploads/2024/08/balatro-1.jpg",
+    },
+    {
+      id: 8,
+      title: "Tekken 7",
+      price: 10000,
+      image: "https://media.graphassets.com/jHYvjBVjT3OnOuPQrTfs",
+    },
+    {
+      id: 9,
+      title: "Tekken 8",
+      price: 10000,
+      image:
+        "https://akinseagleseye.com/wp-content/uploads/2024/02/tekken-8-artwork.jpg",
+    },
+    {
+      id: 10,
+      title: "Among Us",
+      price: 10000,
+      image:
+        "https://static1.srcdn.com/wordpress/wp-content/uploads/2020/11/Among-Us-217-million.jpg",
+    },
   ];
 
   useEffect(() => {
@@ -67,7 +124,10 @@ function App() {
 
     if (lastPromo.date !== today) {
       const randomId = games[Math.floor(Math.random() * games.length)].id;
-      localStorage.setItem("dailyPromo", JSON.stringify({ id: randomId, date: today }));
+      localStorage.setItem(
+        "dailyPromo",
+        JSON.stringify({ id: randomId, date: today })
+      );
       setPromoId(randomId);
     } else {
       setPromoId(lastPromo.id);
@@ -92,14 +152,23 @@ function App() {
 
   return (
     <Router>
-      <Navbar search={search} setSearch={setSearch} user={user} setUser={setUser} />
+      <Navbar
+        search={search}
+        setSearch={setSearch}
+        user={user}
+        setUser={setUser}
+      />
 
       <Routes>
         <Route
           path="/"
           element={
             <>
-              <DailyPoint user={user} discountActive={discountActive} setDiscountActive={setDiscountActive} />
+              <DailyPoint
+                user={user}
+                discountActive={discountActive}
+                setDiscountActive={setDiscountActive}
+              />
               <HomePage
                 discountActive={discountActive}
                 promoId={promoId}
@@ -114,10 +183,17 @@ function App() {
             </>
           }
         />
-        <Route path="/profile" element={<Profile user={user} setUser={setUser} />} />
+        <Route
+          path="/profile"
+          element={<Profile user={user} setUser={setUser} />}
+        />
         <Route path="/login" element={<Login setUser={setUser} />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/cart" element={<CartPage cart={cart} />} />
+        <Route
+          path="/cart"
+          element={<CartPage cart={cart} setCart={setCart} />}
+        />
+
         <Route
           path="/checkout"
           element={
@@ -132,7 +208,12 @@ function App() {
         />
         <Route path="/history" element={<History />} />
         <Route path="/history/:id" element={<TransactionDetail />} />
-        <Route path="/wishlist" element={<Wishlist wishlist={wishlist} toggleWishlist={toggleWishlist} />} />
+        <Route
+          path="/wishlist"
+          element={
+            <Wishlist wishlist={wishlist} toggleWishlist={toggleWishlist} />
+          }
+        />
       </Routes>
 
       <ToastContainer position="top-right" autoClose={2000} />
